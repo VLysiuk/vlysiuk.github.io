7e1e7bac067d5fff327e57ee0b13aa0f899f808a
Volodymyr Lysiuk <karapuzzx@gmail.com>
2020-08-30 21:24:47 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom'; 
+import Home from './Home';
+import About from './About';
+import Blog from './Blog';
+import Resume from './Resume';
+import Contact from './Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/blog" component={Blog} />
+        <Route path="/cv" component={Resume} />
+        <Route path="/contact" component={Contact} />
+
+        <div className="navigation">
+          <img src={logo} className="logo-image" alt="Logo Image" />
+          <div className="navigation-sub">
+            <Link to="/" className="item">Home</Link>
+            <Link to="/about" className="item">About</Link>
+            <Link to="/blog" className="item">Blog</Link>
+            <Link to="/cv" className="item">CV</Link>
+            <Link to="/contact" className="item">Contact</Link>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Blog.js
@@ -0,0 +1,11 @@
+import React from "react"
+ 
+const Blog = (props) => {
+  return (
+    <div>
+      <h1>Articles</h1>
+    </div>
+  )
+}
+ 
+export default Blog;
