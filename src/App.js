03fbeb657763efe57e7097c146c2a583690d96a0
Volodymyr Lysiuk <karapuzzx@gmail.com>
2020-12-20 17:49:01 +0200

    ua tracking id

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import ReactGA from 'react-ga';
 function App() {
 
   useEffect(() => {
-    ReactGA.initialize('G-8KK28161DR');
+    ReactGA.initialize('UA-185877223-1');
     ReactGA.pageview(window.location.pathname);
   })
 
